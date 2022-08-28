e5d31a78cc27e71a7e2cd21f385510e6a7016b31
ankitvijay <ankitvijay@ankit-vm>
2022-08-28 04:17:19 +0000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace container_test_app.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
